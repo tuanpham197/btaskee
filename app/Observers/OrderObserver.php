74e886f1001e7b8944ab9d04be0340858af25906
phamvantuan <tuan.pv@neo-lab.vn>
2023-03-28 22:57:09 +0700

    add point after paid order

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class OrderObserver
+{
+    /**
+     * Handle the Order "created" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function created(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "updated" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function updated(Order $order): void
+    {
+        if ($order->status == Order::ORDER_STATUS_PAID) {
+            $point = ($order->total*5)/100;
+            DB::table('users')->where('id', $order->user_id)
+                ->update([
+                    'point' => floor($point)
+                ]);
+        }
+    }
+
+    /**
+     * Handle the Order "deleted" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function deleted(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "restored" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function restored(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "force deleted" event.
+     *
+     * @param  \App\Models\Order  $order
+     * @return void
+     */
+    public function forceDeleted(Order $order)
+    {
+        //
+    }
+}
